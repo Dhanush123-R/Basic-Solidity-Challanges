4baf8681a8c0b7411fb317e2a61d6b6331c01d19
Dhanush R <111369390+Dhanush123-R@users.noreply.github.com>
2026-02-01 17:31:26 +0530

    Refactor TodoList contract with new task structure

--- TodoList.sol
+++ TodoList.sol
@@ -7,18 +7,71 @@ contract TodoList {
         string startDate;
         string endDate;
         string taskName;
-        string priority;
+        string description;
+        uint256 priority;
         bool isCompleted;
     }
 
+    event TaskCreated(address indexed sender, uint256 taskNumber);
+    event TaskCompleted(address indexed sender, uint256 taskNumber);
+    event TaskDeleted(address indexed sender, uint256 taskNumber);
+    event DescriptionUpdated(address indexed sender, uint256 taskNumber, string Description);
+    event PriorityUpdated(address indexed sender, uint256 taskNumber, uint256 priority);
+    event EndDateUpdated(address indexed sender, uint256 taskNumber, string endDate);
+
     mapping(address => Task[]) tasks;
     
-    function setTasks(string memory _startDate, string memory _endDate, string memory taskName, string memory _priority, bool _isCompleted) public {
-        Task memory newTasks = Task(_startDate, _endDate, taskName, _priority, _isCompleted);
+    function createTasks(string memory _startDate, string memory _endDate, string memory taskName, string memory _description, uint256 _priority, bool _isCompleted) public {
+        Task memory newTasks = Task(_startDate, _endDate, taskName, _description, _priority, _isCompleted);
         tasks[msg.sender].push(newTasks);
+
+        uint256 taskNumber = tasks[msg.sender].length - 1;
+
+        emit TaskCreated(msg.sender, taskNumber);
     }
 
-    function getTasks(address _address, uint256 _taskNumber) public view returns (Task memory) {
-        return tasks[_address][_taskNumber];
+    function isTaskCompleted(uint256 _taskNumber) public {
+        require(_taskNumber < tasks[msg.sender].length, "Task is not present");
+        tasks[msg.sender][_taskNumber].isCompleted = !tasks[msg.sender][_taskNumber].isCompleted;
+
+        emit TaskCompleted(msg.sender, _taskNumber);
     }
+
+    function deleteTasks(uint256 _taskNumber) public {
+        uint256 lastTask = tasks[msg.sender].length -1;
+        require(tasks[msg.sender].length > 0, "List is already empty");
+        require(_taskNumber <= lastTask, "Task does not exist");
+
+        tasks[msg.sender][_taskNumber] = tasks[msg.sender][lastTask];
+        tasks[msg.sender].pop();
+
+        emit TaskDeleted(msg.sender, _taskNumber);
+    }
+
+    function extendEndDate(uint256 _taskNumber, string memory _newEndDate) public {
+        require(!tasks[msg.sender][_taskNumber].isCompleted, "Task is already done");
+        tasks[msg.sender][_taskNumber].endDate = _newEndDate;
+        emit EndDateUpdated(msg.sender, _taskNumber, _newEndDate);
+    }
+
+    function updateDescription(uint256 _taskNumber, string memory _newDescription) public {
+        require(!tasks[msg.sender][_taskNumber].isCompleted, "Task is already done");
+        tasks[msg.sender][_taskNumber].description = _newDescription;
+        emit DescriptionUpdated(msg.sender, _taskNumber, _newDescription);
+    }
+
+    function updatePriority(uint256 _taskNumber, uint256 _newPriority) public {
+        require(!tasks[msg.sender][_taskNumber].isCompleted, "Task is already done");
+        tasks[msg.sender][_taskNumber].priority = _newPriority;
+        emit PriorityUpdated(msg.sender, _taskNumber, _newPriority);
+    }
+
+    function getTasksCount() public view returns (uint256) {
+        return tasks[msg.sender].length;
+    }
+
+    function getTasks(uint256 _taskNumber) public view returns (Task memory) {
+        return tasks[msg.sender][_taskNumber];
+    }
+
 }
